e583fac55a0dde86fb7e68ce7abad2a4da05ce80
Muhammet Gumus <muhammetgms95@gmail.com>
2022-08-13 22:40:11 +0300

    LongestSubstringWithoutRepeatingChars added

---
+++ java/src/Q3_LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,32 @@
+package src;
+
+import java.nio.file.LinkPermission;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+
+public class Q3_LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("dvdf"));
+        System.out.println(lengthOfLongestSubstring("abcadb"));
+    }
+
+
+    public static int lengthOfLongestSubstring(String input) {
+        if (input.length() == 0) {
+            return 0;
+        }
+        int i = 0, j = 0, max = 0;
+        HashSet<Character> chSet = new HashSet<>();
+        while (j < input.length()) {
+            if (!chSet.contains(input.charAt(j))) {
+                chSet.add(input.charAt(j++));
+                max = Math.max(max, chSet.size());
+            } else {
+                chSet.remove(input.charAt(i++));
+            }
+        }
+        return max;
+    }
+}
