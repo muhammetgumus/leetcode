9587a834be6f5d012a8152417f89f474c5b1dccf
Muhammet Gumus <muhammetgms95@gmail.com>
2022-08-14 19:10:53 +0300

    Q28-ImplementStrStr solution added

---
+++ java/src/Q28_ImplementStrStr.java
@@ -0,0 +1,15 @@
+package src;
+
+import java.util.Locale;
+
+public class Q28_ImplementStrStr {
+    public static void main(String[] args) {
+        Q28_ImplementStrStr obj = new Q28_ImplementStrStr();
+        System.out.println(obj.strStr("aaaaa",""));
+    }
+    public int strStr(String haystack, String needle) {
+        if(haystack == "" || needle == "")
+            return 0;
+        return haystack.toLowerCase().indexOf(needle.toLowerCase());
+    }
+}
