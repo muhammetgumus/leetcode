0007522348737bad67d56268cc916455b773a10d
Muhammet Gumus <muhammetgms95@gmail.com>
2023-05-30 21:33:21 +0300

    Q237 - Delete Node in LinkedList solution added

---
+++ java/src/Q237_DeleteNodeInLinkedList.java
@@ -0,0 +1,34 @@
+package src;
+
+public class Q237_DeleteNodeInLinkedList {
+    public static void main(String[] args) {
+        ListNode head =  new ListNode(4);
+        ListNode node1 =  new ListNode(5);
+        ListNode node2 =  new ListNode(1);
+        ListNode node3 =  new ListNode(9);
+
+        head.next=node1;
+        node1.next=node2;
+        node2.next=node3;
+        node3.next=null;
+
+        deleteNode(node1);
+
+        while(head!=null){
+            System.out.println(head.val);
+            head=head.next;
+        }
+    }
+
+    public static void deleteNode(ListNode node) {
+        node.val= node.next.val;
+        node.next= node.next.next;
+    }
+
+    static class ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) { val = x; }
+      }
+
+}
